855b3d9305cce9df787ebf62a329b62cfbe4bb09
root <root@binwon>
2024-02-18 19:39:34 +0900

    update https server config

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ubuntu/Test_File/myvenv"
+set -gx VIRTUAL_ENV "/root/Test_File/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
