3623a263d046e3ab0c328abe15040dd3fed620fc
이동민 <idongmin@idongmin-ui-MacBookAir.local>
2024-07-20 14:32:39 +0900

    feat 테케 수정

--- testweb/testsel/views.py
+++ testweb/testsel/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
-from .selenium_list import process_click_xpath, process_click_xpath_otherurl, process_click_xpath_div
-# Create your views here.
-
 import json
-from django.views.decorators.csrf import csrf_exempt
 from .models import TestSave
+from .selenium_list import (
+    process_click_xpath, process_click_xpath_otherurl, 
+    process_click_xpath_div, process_click_xpath_iframe,
+    process_send_xpath
+)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 class BaseView(TemplateView):
     template_name = 'base.html'
@@ -15,91 +22,96 @@ class ProcessView(TemplateView):
     template_name = 'process.html'
 
     def post(self, request, *args, **kwargs):
-
         processed_data_list = []
         data = json.loads(request.body.decode('utf-8'))
         action_type = data.get('action_type', '')
-        
+
         if action_type == 'test':
             try:
-                dynamic_inputs_list = data.get('dynamic_inputs', [])
-                for data in dynamic_inputs_list:
-                    process_type = data.get('type')
+                chrome_options = Options()
+                chrome_options.add_experimental_option("detach", True)
+                driver = webdriver.Chrome(options=chrome_options)
 
-                    # Assuming functions are imported from selenium_list.py
+                main_url = data.get('main_url', '')
+                driver.get(main_url)
+
+                # 메인 페이지 로드 확인
+                try:
+                    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+                    main_page_status = "성공"
+                except TimeoutException:
+                    main_page_status = "시간초과"
+                
+                dynamic_inputs_list = data.get('dynamic_inputs', [])
+                for item in dynamic_inputs_list:
+                    process_type = item.get('type')
+                    target = item.get('target')
+                    input_data = item.get('input')
+                    result = item.get('result')
+                    iframe_xpath = item.get('iframe_xpath', '')  # 추가
+
                     if process_type == 'process_click_xpath':
-                        print("1번실행")
-                        processed_data = process_click_xpath(
-                            data.get('url'),
-                            data.get('target'),
-                            data.get('input'),
-                            data.get('result')
-                        )
+                        processed_data = process_click_xpath(driver, main_url, target, input_data, result)
                     elif process_type == 'process_click_xpath_otherurl':
-                        print("2번실행")
-                        processed_data = process_click_xpath_otherurl(
-                            data.get('url'),
-                            data.get('target'),
-                            data.get('input'),
-                            data.get('result')
-                        )
+                        processed_data = process_click_xpath_otherurl(driver, main_url, target, input_data, result)
                     elif process_type == 'process_click_xpath_div':
-                        print("3번실행")
-                        processed_data = process_click_xpath_div(
-                            data.get('url'),
-                            data.get('target'),
-                            data.get('input'),
-                            data.get('result')
-                        )
+                        processed_data = process_click_xpath_div(driver, main_url, target, input_data, result)
+                    elif process_type == 'process_click_xpath_iframe':
+                        processed_data = process_click_xpath_iframe(driver, main_url, target, input_data, result, iframe_xpath)
+                    elif process_type == 'process_send_xpath':
+                        processed_data = process_send_xpath(driver, main_url, target, input_data, result)
                     else:
                         processed_data = f"Unsupported process type: {process_type}"
 
                     processed_data_list.append({
                         'type': process_type,
-                        'url': data.get('url'),
-                        'result': data.get('result'),
+                        'target': target,
+                        'input': input_data,
+                        'result': result,
                         'processed_data': processed_data,
                     })
 
+                driver.quit()
                 return JsonResponse({'processed_data_list': processed_data_list})
 
             except json.JSONDecodeError:
-                # Handle JSON decoding error
-                pass
+                return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
-            return JsonResponse({'processed_data_list': []})
         elif action_type == 'save':
-            # Handle the 'save' action type
             save_data_list = []
+            main_url = data.get('main_url', '')
+            test_name = data.get('test_name', '')  # 추가
             dynamic_inputs_list = data.get('dynamic_inputs', [])
-            for data in dynamic_inputs_list:
+            for item in dynamic_inputs_list:
                 test_save_instance = TestSave(
-                    type=data.get('type', ''),
-                    url=data.get('url', ''),
-                    target=data.get('target', ''),
-                    input=data.get('input', ''),
-                    result=data.get('result', ''),
-                    test_num = '1', #임시값 추후 사용자를 완성하면 추가할 예정 
-                    test_uid = '1', #임시값 추후 사용자를 완성하면 추가할 예정
+                    test_name=test_name,  # 추가
+                    type=item.get('type', ''),
+                    url=main_url,
+                    target=item.get('target', ''),
+                    input=item.get('input', ''),
+                    result=item.get('result', ''),
+                    test_num='1',  # 임시값
+                    test_uid='1',  # 임시값
                 )
                 test_save_instance.save()
 
                 save_data_list.append({
-                    'saved_data': 'Save successful for input: {}'.format(data),
+                    'saved_data': f"Save successful for input: {item}",
                 })
 
             return JsonResponse({'save_data_list': save_data_list})
-        elif action_type == 'load':  # 
+
+        elif action_type == 'load':
             test_names = TestSave.objects.values_list('test_name', flat=True).distinct()
             return JsonResponse({'test_names': list(test_names)})
+
         elif action_type == 'db_load':
             selected_test_names = data.get('selectedTests', [])
             filtered_tests = TestSave.objects.filter(test_name__in=selected_test_names).values()
             return JsonResponse({'data': list(filtered_tests)})
 
         else:
-            return JsonResponse({'error': 'Unsupported action type'})
+            return JsonResponse({'error': 'Unsupported action type'}, status=400)
 
-    
 class RecordView(TemplateView):
     template_name='test.html'
