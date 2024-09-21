684841f47178f2c03a084de5d243bc9b007b2ae2
이동민 <idongmin@idongmin-ui-MacBookAir.local>
2024-09-21 14:43:06 +0900

    fix : error

--- testweb/testrecord/urls.py
+++ testweb/testrecord/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import RecordView
 from .views import save_record
 urlpatterns = [
-    path('', RecordView.as_view(), name='record_view'),
     path('save/', save_record, name='save_db'),
 ]
